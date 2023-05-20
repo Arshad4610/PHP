856906fc256701690cdc5bdeebfdd5b8cf8dd703
Arshad4610 <134123830+Arshad4610@users.noreply.github.com>
2023-05-20 11:14:01 +0530

    Curriculum Vitae

---
+++ curvit.php
@@ -0,0 +1,95 @@
+<html>
+
+<title>Resume</title>
+
+<body>
+
+<pre><font face="arial black">VAIDOORIYA. M, vaidoo.msc@gmail.com
+Kenikarai,	9999 999 999
+
+Ramanathapuram.</font></pre><hr>
+
+<h2><u><font color="orange">JOB OBJECTIVE:</font></u></h2>
+
+<p>A website developer position where advanced creative skills will produce a quality website for the company.</p>
+<h2><u><font color="orange">ACADEMIC PROFILE:</font></u><br></h2>
+
+<h4><font color="green">POST GRADUATION:</font></h4>
+
+<pre>
+
+<b>Course</b>	: M.Sc(Computer Science)
+
+<b>University</b>	: Alagappa University
+
+<b>College</b>	: Government Arts College for Women,Ramanathapuram.
+
+<b>Year of Passing</b> : 2015-2017
+
+<b>Percentage</b>	: Pursuing</pre>
+
+<h4><font color="green">UNDER GRADUATION:</font></h4>
+
+<pre>
+
+<b>Course</b>	: B.Sc(Computer Science)
+
+<b>University</b>	: Alagappa University
+
+<b>College</b>	: Syed Ammal Arts & Science College,Ramanathapuram.
+
+<b>Year of Passing</b> : 2012-2015
+ 
+<b>Percentage</b>	: 76%</pre>
+
+<h4><font color="green">HIGHER SECONDARY:</font></h4>
+
+<pre>
+
+<b>School</b>	: D.D.Vinayagar Hr.Sec. School
+
+<b>Board</b>	: State Board
+
+<b>Year of Passing</b> : 2010-2012
+
+<b>Percentage</b>	: 82%</pre>
+
+<h3><font color="green">SSLC:</font></h3>
+
+<pre>
+
+<b>School</b>	: Raja Dhinagar R.C High School
+
+<b>Board</b>	: State Board
+
+<b>Year of Passing</b> : 2009-2010
+
+<b>Percentage</b>	: 88.4%</pre>
+
+<h2><u><font color="orange">ACHIEVEMENTS:</font></u></h2>
+
+<ul>
+
+<li>Certificate of Merit in SHIBUKAN SHITO - RYU KARATE -DO, <b>District Open Championship</b> -2010.
+<li><b>First prize</b> on Rotary Club <b>Fashion Show, District Level</b> -2012.
+
+<li><b>First prize</b> on Intercollegiate Met Excel College for <b>Fashion Show, National Level</b> - 2013.</ul>
+<h2><u><font color="orange">ADDITIONAL QUALITFICATION:</font></u></h2>
+
+<ul>
+
+<li>Certificate of Proficiency in <b>PEC</b>, Binx Computer Education.
+
+<li>Type writing <b>Senior</b> Grade <b>First Class</b> in English</ul>
+
+<h2><u><font color="orange">DECLARATION:</font></u></h2>
+
+<p>I hereby declare that the details provided above are true to the best of my knowledge.</p><br><br>
+
+<pre>
+
+<b>Place:</b> Ramanathapuram	<b>Yours faithfully,</b>
+
+<b>Date :</b> 22.7.16	<font face="Brush Script MT">(M. Vaidooriya)</font>
+
+</body></html>
