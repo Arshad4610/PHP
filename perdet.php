8f4eec95e6beb958f90cf4a6e1219ab778cde5fb
Arshad4610 <134123830+Arshad4610@users.noreply.github.com>
2023-05-20 11:10:29 +0530

    Create perdet.php

---
+++ perdet.php
@@ -0,0 +1,23 @@
+<html>
+
+<body text="red" bgcolor="orange"><table align="center" bgcolor="white"><tr><td>
+
+<center><h1><hr><hr>** PERSONAL DETAILS **<hr><hr></h1></center>
+
+<font color="blue"><pre><h3>	NAME	- <b><font face="Courier">Riya</font>
+
+D.O.B	- <b><font face="Courier">Sep 10,1995</font> BLOOD GROUP - <b><font face="Courier">A+</font> ADDRESS		- <b><font face="Courier">Kenikarai</font> CONTACT NO	- <b><font face="Courier">04567-220023</font>
+EMAIL ID	- <b><font face="Courier">riya.msc@gmail.com</font></font>
+
+<font color="deeppink"><center><hr><hr><h1> ** MY FAVOURITES **</h1></center>
+
+<hr><hr><font color="green">	FRIEND	: <b><font face="Courier"> Akila</font> FOOD	: <b><font face="Courier"> Idly</font>
+COLOUR			: <b><font face="Courier">Blue & Pink</font> PLACE	: <b><font face="Courier">USA</font> SPORTS		: <b><font face="Courier"> Football</font>
+DIRECTOR	: <b><font face="Courier">Gowtham vasudev menan</font>
+
+BOOK	: <b><font face="Courier">The MONK Who Sold His FERRARI</font> MOVIE		: <b><font face="Courier">Ok Ok</font>
+HOBBIES	: <b><font face="Courier">Drawing </font>
+
+</font></pre></h3></td></tr></table>
+
+</body></html
